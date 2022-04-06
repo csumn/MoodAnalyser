3c8dea1e507c461fa1f7a3c2b929b04ed2e85438
csumn <csumn13@gmail.com>
2022-04-06 15:26:53 +0530

    Handled empty mood and test case written

--- MoodAnalyser/src/test/java/com/bridgelabz/MoodAnalyserTest.java
+++ MoodAnalyser/src/test/java/com/bridgelabz/MoodAnalyserTest.java
@@ -6,19 +6,44 @@ import org.junit.Test;
 public class MoodAnalyserTest {
 
 	@Test
-	public void givenMessage_WhenNotSad_ShouldReturnHappy(){
+	public void givenMessage_WhenNotSad_ShouldReturnHappy() {
 		MoodAnalyser moodanalyser = new MoodAnalyser("I'm in Any mood");
-		Assert.assertEquals("HAPPY",moodanalyser.analyseMood());
+		try {
+			Assert.assertEquals("HAPPY", moodanalyser.analyseMood());
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 	@Test
-	public void givenMessage_WhenSad_ShouldReturnSad(){
+	public void givenMessage_WhenSad_ShouldReturnSad() throws MoodAnalyserException {
 		MoodAnalyser moodAnalyser = new MoodAnalyser("I'm in a Sad mood");
-		Assert.assertEquals("SAD",moodAnalyser.analyseMood());
+		try {
+			Assert.assertEquals("SAD", moodAnalyser.analyseMood());
+		} catch (MoodAnalyserException e) {
+			e.printStackTrace();
+		}
 	}
+
 	@Test
-	public void givenMessage_WhenNull_ReturnHappy(){
+	public void givenMessage_WhenNull_ReturnHappy() {
 		MoodAnalyser moodAnalyser = new MoodAnalyser(null);
-		Assert.assertEquals("HAPPY",moodAnalyser.analyseMood());
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalyserException e) {
+			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_NULL, e.type);
+		}
+	}
+
+	@Test
+	public void givenMessage_WhenEmpty_ThrowException() {
+		MoodAnalyser moodAnalyser = new MoodAnalyser("");
+
+		try {
+			moodAnalyser.analyseMood();
+		} catch (MoodAnalyserException e) {
+			Assert.assertEquals(MoodAnalyserException.ExceptionType.ENTERED_EMPTY, e.type);
+		}
 	}
 }
